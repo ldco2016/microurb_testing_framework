8c49ace2c4616cc5e1fcd2eb2617c2ee7dca3708
ldco2016 <renaissance.scholar2012@gmail.com>
2020-09-03 23:17:35 -0500

    HTML element assertion

---
+++ samplewebproject/test/app.test.js
@@ -0,0 +1,11 @@
+const { italic } = require("chalk");
+
+const assert = require("assert");
+
+it("has a text input", async () => {
+  const dom = await render("index.html");
+
+  const input = dom.window.document.querySelector("input");
+
+  assert(input);
+});
